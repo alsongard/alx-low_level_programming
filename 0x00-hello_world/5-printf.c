79d6f93c608468c73e39f64afecb4f1ada10333f
alsongard <alsongard@gmail.com>
2023-07-13 17:07:56 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int_start()
+/*
+ * main -Entry Point
+ *
+ * Return always 0 (Success)
+ */
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
