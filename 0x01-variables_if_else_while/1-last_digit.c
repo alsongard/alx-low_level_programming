968b9c4468e1609bd6d5d80f8eecf9dca1c7ee74
alsongard <alsongard@gmail.com>
2023-07-15 13:28:55 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /*
  * main - Entry point
  * Description - compare the random generated interger
@@ -17,15 +16,15 @@ int main(void)
 	lastDgt = n % 10;
 	if (lastDgt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5.\n", n, lastDgt);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDgt);
 	}
 	else if (lastDgt == 0)
 	{
-		printf("Last digit of %d is %d and is 0.\n", n, lastDgt);
+		printf("Last digit of %d is %d and is 0\n", n, lastDgt);
 	}
 	else if (lastDgt < 6 && lastDgt != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0.\n", n, lastDgt);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDgt);
 	}
 	return (0);
 }
