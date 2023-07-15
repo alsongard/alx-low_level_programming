cd196b428c498a4f42f0e3c18aada5dc6464f5dc
alsongard <alsongard@gmail.com>
2023-07-15 14:01:18 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main - Entry point/start program
+ * main - used to compare a digit to three options
  * Description - compare the random generated interger
  * Return: 0 (Successfull)
  */
