3a30ae8aee9622ab3ac838af190862d9bc7552b6
alsongard <alsongard@gmail.com>
2023-07-15 13:09:34 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
-#include<stdio.h>
+#include <stdio.h>
 
 /*
  * main - Entry point
- * Description - the proram is used to compare the random generated interger by three options
+ * Description - compare the random generated interger
  * Return: 0 (Successfull)
  */
 int main(void)
 {
 	int n;
-	
+	int lastDgt;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (r > 5)
+	/* your code goes there */
+	lastDgt = n % 10;
+	if (lastDgt > 5)
 	{
-	printf("Last digit of %i is %i and is greater than 5\n", n, r);
+		printf("Last digit of %d is %d and is greater than 5.\n", n, lastDgt);
 	}
-	else if (r == 0)
+	else if (lastDgt == 0)
 	{
-	printf("Last digit of %i is %i and is 0\n", n, r);
+		printf("Last digit of %d is %d and is 0.\n", n, lastDgt);
 	}
-	else
+	else if (lastDgt < 6 && lastDgt != 0)
 	{
-	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, r);
+		printf("Last digit of %d is %d and is less than 6 and not 0.\n", n, lastDgt);
 	}
 	return (0);
 }
