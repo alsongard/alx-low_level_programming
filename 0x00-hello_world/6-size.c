6dab91f38411afa4e0baffa732ae7305c2b25e22
alsongard <alsongard@gmail.com>
2023-07-15 21:32:09 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Print size of data tyoes
+ *
+ * description - print size of data types
+ * return: 0 (Success)
+ */
+
+int main(void)
+{
+        printf("Size of a char: %2d byte(s)\n", sizeof(char));
+        printf("size of an int: %2d byte(s)\n", sizeof(int));
+        printf("size of a long int: %2d byte(s)\n", sizeof( int));
+        printf("size of an long long int: %2d byte(s)\n", sizeof(long int));
+        printf("size of a float: %2d byte(s)\n", sizeof(float));
+        return (0);
+}
