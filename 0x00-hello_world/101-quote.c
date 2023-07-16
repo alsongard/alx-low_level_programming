ef9b31d6ed8e24313e1c10086c445f9e898a233a
alsongard <alsongard@gmail.com>
2023-07-15 22:34:55 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Display text exactly and with stderror
+ *
+ * Description - display script
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Write a C program that prints exactly\n");
+	return (1);
+}
