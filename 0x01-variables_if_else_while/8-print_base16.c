5067f8a4a87de22c0d8c100252ed2d075087438c
alsongard <alsongard@gmail.com>
2023-07-15 17:48:05 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main -display all hexadecimal
+ *
+ * description - display all values of hex
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char num;
+	char let;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		putchar(num);
+	}
+	for (let = 'A' ; let <= 'F' ; let++)
+	{
+		putchar(let);
+	}
+	putchar('\n');
+	return (0);
+}
