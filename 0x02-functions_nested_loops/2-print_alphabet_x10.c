6347f0944364efd10d3569d0794fd8216c6c52b0
alsongard <alsongard@gmail.com>
2023-07-18 14:06:24 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main -print all alphabet
+ *
+ * Description - print all alphabets 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char  let;
+	int line = 0;
+
+	while (line < 10)
+	{
+		for (let = 'a' ; let <= 'z' ; let++)
+		{
+			_putchar(let);
+
+		}
+
+		_putchar('\n');
+		line++;
+
+	}
+}
