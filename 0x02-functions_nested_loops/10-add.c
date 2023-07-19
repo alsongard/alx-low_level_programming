d93ebc5b3520cf1e0ceb08f450cabf156056dee6
alsongard <alsongard@gmail.com>
2023-07-18 18:55:47 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * @n: the two numbers to add
+ * Description -  add two numbers
+ * Return: (Sum)
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum =  num1 + num2;
+	_putchar(sum + '0');
+	return (sum);
+}
