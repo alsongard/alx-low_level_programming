bb005354ba6b32b99838438e0b85a3732cb566e9
alsongard <alsongard@gmail.com>
2023-07-18 18:02:27 -0700

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * _abs - Check absolute number of int
  *
  * @c:  integer to be compared
@@ -12,8 +11,8 @@ int _abs(int c)
 	if (c < 0)
 	{
 		int abs_val;
-		
-		abs_val = c * -1;
+
+		abs_val = (c * -1)
 		return (abs_val);
 	}
 	return (c);
