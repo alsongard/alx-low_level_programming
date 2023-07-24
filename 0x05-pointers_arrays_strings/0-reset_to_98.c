0bea73388b467a0601b76472bb58096e71f633f3
alsongard <alsongard@gmail.com>
2023-07-24 14:03:08 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - the function will reset value
+ * @n: integer value which is to be replaced
+ *
+ * desription - replace value of n with 98
+ * return: (void)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
