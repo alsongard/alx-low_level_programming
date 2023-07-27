75a65c4afbea9df33ce29e05fd61cf8919e86813
alsongard <alsongard@gmail.com>
2023-07-27 05:44:31 -0700

    readme

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - copy string and append
+ * @dest: pointer to character to be added text
+ * @src: pointer to var charcater for text to copy
+ *
+ * Return: (Character)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
