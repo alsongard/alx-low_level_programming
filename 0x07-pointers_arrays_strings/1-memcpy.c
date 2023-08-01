fb0b90d27c8ae9cf09ede9cdcfa576c8f563757f
alsongard <alsongard@gmail.com>
2023-07-31 17:49:11 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memcpy -  copy data from src to dest
+ * @src:  pointer to variable for source data
+ * @dest:  pointer to variable for dest data
+ * @n: integr to be used for limit
+ *
+ * Return: (Character)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+}
