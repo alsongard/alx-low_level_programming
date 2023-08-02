921a9d239c7c4514f7fa5c8724b539fe266e5f11
alsongard <alsongard@gmail.com>
2023-08-01 18:09:45 -0700

    task8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - used to copy istring to another string
+ * @s: double pointer used for copy data
+ * @to: pointer to character to be set string
+ * Return: (void)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
