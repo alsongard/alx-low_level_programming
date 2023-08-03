4c3c0fb17ca6bcbc7195f0edef8d2f06554dde86
alsongard <alsongard@gmail.com>
2023-08-02 17:30:50 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - print length of string
+ * @s: pointer ot string
+ * Return: (void)
+ */
+int _strlen_recursion(char *s)
+{
+	 if (*s == '\0')
+	 {
+		 return (0);
+	 }
+	 s++;
+	 return (_strlen_recursion(s) +1);
+}
