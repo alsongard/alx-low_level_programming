6f221f3abcdaa5eecd660632818d5cb0050c97e5
alsongard <alsongard@gmail.com>
2023-08-09 18:13:50 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,13 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int a, b;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	a = 0;
 	b = 0;
