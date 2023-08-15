0799a8c6bdbbedc4d1b4ee5f9466a1d818080467
alsongard <alsongard@gmail.com>
2023-08-15 13:57:17 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - stucture for name, age and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char name;
+	char owner;
+	float age;
+}
