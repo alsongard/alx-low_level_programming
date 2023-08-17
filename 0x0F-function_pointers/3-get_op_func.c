5c10e6d64bcb4ead3bde77a531efa5e1d7ef09f0
alsongard <alsongard@gmail.com>
2023-08-16 20:01:32 -0700

    task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stdlib.h>
 /**
  * get_op_func - select function based on user
  * @s: the operator to be selected
@@ -7,16 +8,16 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add};
-		{"-", op_sub};
-		{"*", op_mul};
-		{"/", op_div};
-		{"%", op_mod};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
-	}
+	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op != *s))
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		i++;
 	}
