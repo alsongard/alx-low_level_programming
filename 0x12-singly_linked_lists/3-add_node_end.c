8ad49a7943d4dc408ab1719369eea3860d6bcca3
alsongard <alsongard@gmail.com>
2023-08-25 14:19:55 -0700

    0x12

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
 	size_t n;
-	
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
@@ -19,7 +19,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 
-	for (n = 0 ; str[n] ; n++);
+	for (n = 0 ; str[n] ; n++)
+		;
+
 	new_node->len = n;
 	new_node->next = NULL;
 	current_node = *head;
