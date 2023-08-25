2a25c46f607d4131099e8f3ecf5f659f31e56b44
alsongard <alsongard@gmail.com>
2023-08-24 17:45:01 -0700

    0x12-linkedlists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "liss.h"
+#include <stdio.h>
+/**
+ * print_list - print all elements of a linked lists
+ * @h: pointer to the list_t list to print
+ * Return: number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->sr)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
